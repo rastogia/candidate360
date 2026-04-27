83021b94fbb28780806b48cfa2c2081b4bfc6d3a
rastogia <rassu.akash@gmail.com>
2026-04-27 18:59:34 +0530

    Add src/test/java/com/recruit/c360/Candidate360ApplicationTests.java

---
+++ src/test/java/com/recruit/c360/Candidate360ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.recruit.c360;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+@SpringBootTest
+@ActiveProfiles("test")
+class Candidate360ApplicationTests {
+    @Test
+    void contextLoads() {}
+}
